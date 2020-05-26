74117ef97dfec2d2cdcb6753990d09ee52516f23
BluABK <bluabk@bluabk.net>
2020-05-26 14:46:51 +0200

    Added support for omitting added tags in the case submitted to YARA Designer.

--- responder/yara_designer_responder.py
+++ responder/yara_designer_responder.py
@@ -15,7 +15,7 @@ class YaraDesignerResponder(Responder):
 
         self.operations_list = []
         self.endpoint = self.get_param(
-            "config.designer_core_endpoint",
+            "config.endpoint",
             message="Endpoint is not configured, aborting!")
 
         # Input will be on the form of a list of strings.
@@ -24,6 +24,8 @@ class YaraDesignerResponder(Responder):
 
         # Input will be on the form of a list of stringified JSON objects.
         [self.add_operation(op) for op in self.get_param("config.custom_operations")]
+
+        self.send_added_tags = self.get_param("config.send_added_tags", default=False)
 
     def add_operation(self, operation: str):
         """
@@ -56,14 +58,25 @@ class YaraDesignerResponder(Responder):
         if tag:
             self.operations_list.append({"type": "AddTagToCase", "tag": tag})
 
+    def get_added_tags(self) -> list:
+        """Returns a list of all the custom added tags specified in config"""
+        return [js["tag"] for js in self.operations_list if "tag" in js]
+
     def run(self):
         if self.data_type != TH_DATATYPE_CASE:
             self.error("Invalid dataType: got '{data_type}', expected '{case_data_type}'!".format(
                 data_type=self.data_type, case_data_type=TH_DATATYPE_CASE))
 
         try:
+            # Make a copy of the case data (to allow for modifications that don't persist).
+            case = self.get_data()
+
+            if self.send_added_tags is False:
+                # Strip all the custom tags from the copied case's tags list.
+                case["tags"] = [tag for tag in case["tags"] if tag not in self.get_added_tags()]
+
             # POST data to YARA Designer core endpoint.
-            r = requests.post(self.endpoint, json=self.get_data(), headers={'Content-type': 'application/json'})
+            r = requests.post(self.endpoint, json=case, headers={'Content-type': 'application/json'})
 
             if r.status_code != 200:
                 self.error("POST Request to {endpoint} failed with status: {code} {reason}! Data: {data}".format(
@@ -76,6 +89,7 @@ class YaraDesignerResponder(Responder):
                 "message": "Case successfully sent to YARA Designer.",
                 "destination_endpoint": self.endpoint,
                 "operations": self.operations_list,
+                "send_added_tags": self.send_added_tags,
                 "in": self._input,
                 "out": json.loads(r.text)
             })
